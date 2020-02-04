64116c5d4667f68e3734dd4f197e7d1bac316480
Rares Ont <raresont@gmail.com>
2020-02-04 23:39:26 +0200

    added transactional button

--- src/app/pages/tabs-page/tabs-page.ts
+++ src/app/pages/tabs-page/tabs-page.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: 'tabs-page.html'
 })
-export class TabsPage {}
+export class TabsPage {
+
+  constructor(private router: Router){}
+  startRecord() {
+    return this.router.navigateByUrl('/app/tabs/speakers/speaker-details/1');
+  }
+}
